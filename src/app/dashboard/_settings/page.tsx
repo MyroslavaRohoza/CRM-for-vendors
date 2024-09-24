702de1d035100593d922b9f4b0d92adbcbb2e117
Myroslava Rogoza <myroslavarogoza@gmail.com>
2024-09-24 16:49:25 +0300

    File-System Based Routing

---
+++ src/app/dashboard/_settings/page.tsx
@@ -0,0 +1,11 @@
+export interface PageProps {
+  children: React.ReactNode;
+}
+
+export default function ComponentName() {
+  return (
+    <main>
+      <h1 className="text-xl">Settings Page</h1>
+    </main>
+  );
+}
---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+export interface PageProps {
+  children: React.ReactNode;
+}
+
+export default function ComponentName() {
+  return (
+    <main>
+      <h1 className="text-xl">Dashboard Page</h1>
+    </main>
+  );
+}
